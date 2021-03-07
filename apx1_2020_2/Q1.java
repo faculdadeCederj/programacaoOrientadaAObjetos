cdd2c241b9a7df518002fd953cf26185d2be29ab
William Souza <williamguilhermesouza@gmail.com>
2021-03-07 15:44:13 -0300

    Add files via upload

---
+++ apx1_2020_2/Q1.java
@@ -0,0 +1,40 @@
+package apx1_2020_2;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String line = scan.nextLine();
+        while(!line.equals("FIM")) {
+            String[] lineArray = line.split(" ");
+            int indexOperators = 0, indexValues = 0;
+            char[] operators = new char[lineArray.length];
+            int[] values = new int[lineArray.length];
+            for (int i = 0; i < lineArray.length; i++) {
+                if (lineArray[i].equals("("));
+                else if (lineArray[i].equals("+") ||
+                        lineArray[i].equals("-") ||
+                        lineArray[i].equals("*") ||
+                        lineArray[i].equals("/") ||
+                        lineArray[i].equals("%")) {
+                    operators[indexOperators++] = lineArray[i].charAt(0); // chartAt returns a char instead of string
+                } else if (lineArray[i].equals(")")) {
+                    char operator = operators[--indexOperators];
+                    int value = values[--indexValues];
+                    if (operator == '+') value = values[--indexValues] + value;
+                    else if (operator == '-') value = values[--indexValues] - value;
+                    else if (operator == '*') value = values[--indexValues] * value;
+                    else if (operator == '/') value = values[--indexValues] / value;
+                    else if (operator == '%') value = values[--indexValues] % value;
+                    values[indexValues++] = value;
+                } else values[indexValues++] = Integer.parseInt(lineArray[i]);
+            }
+            int result = values [--indexValues];
+            System.out.println(line + " = " + result);
+            line = scan.nextLine();
+        }
+        scan.close();
+    }
+}
+
